de75e795c3a9ed94b835c312bf174ca6e5aee832
Misha Berrien <misha.berrien@gmail.com>
2020-03-17 11:57:45 -0500

    continued to explore dataset with visualizations

--- data/01-bq_files.R
+++ data/01-bq_files.R
@@ -55,5 +55,20 @@ students <-
          geocode,
          entrydate,
          schoolentrydate,
-         schoolentrygradelevel) %>%
+         schoolentrygradelevel, 
+         exitcode, 
+         exitcomment) %>%
   collect()
+
+school_id_abbr <- 
+  tribble(
+    ~schoolid, ~abbr,
+    "78102", "KAP",
+    "7810", "KAMS",
+    "400146", "KAC",
+    "4001462", "KACP",
+    "4001632", "KBP",
+    "400163", "KBCP",
+    "4001802", "KOP",
+    "400180", "KOA",
+  )
